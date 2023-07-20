d43e2542d2a0eab6cf89975e736d0822408b27a3
jojazpeitia <jojazpeitia@gmail.com>
2023-07-19 21:29:48 -0500

    got rid of the math stuff

--- src/app/cs/page.tsx
+++ src/app/cs/page.tsx
@@ -1,67 +1,54 @@
 'use client'
 
-import { useState, useEffect } from 'react';
-import Image from 'next/image';
+import { useState } from 'react';
+import Image from 'next/image'
 
 export default function CSPage() {
-    // const [initialZoom, setInitialZoom] = useState(210);
-    const [initialZoom, setInitialZoom] = useState(28);
+    
+    // sets initial zoom to a value of 28.
+    const [initialZoom, setInitialZoom] = useState(28); 
 
-    const imageWidth = 4096; // Width of the original image
-    const imageHeight = 4096; // Height of the original image
-    const imageContainerWidth = 512; // Adjust this value based on the desired container width
+    // const xAxisMovement = -210; 
+    // const yAxisMovement = -250; 
+    const xAxisMovement = 0;  // center x value 
+    const yAxisMovement = 0; // center y value 
 
-    // Calculate the initial scale factor based on the container and image dimensions
-    const initialScale = imageContainerWidth / imageWidth;
-
-    // Calculate the initial X and Y axis movement to center the image
-    const xAxisMovement = ((imageWidth * initialScale - imageContainerWidth) / 2);
-    const yAxisMovement = ((imageHeight * initialScale - imageContainerWidth) / 2);
-
-    // Sets variables for scale, x and y. This allows us to manipulate the values 
-    // and adjust the sizes dynamically
+    
     const imageStyle = {
-        transform: `scale(${initialZoom * initialScale}) translateX(${-xAxisMovement}px) translateY(${-yAxisMovement}px)`,
+        width: '640', // matches the height of the max-w-screen
+        height: '640', // matches the height of the h-max i think
+        transform: `scale(${initialZoom}) translateX(${xAxisMovement}px) translateY(${yAxisMovement}px)`,
         transition: 'transform 0.3s ease-in-out',
     };
 
-    // We use the useEffect hook to update the initial zoom level 
-    // whenever the container width or the image width changes to keep the zoom consistent.
-    useEffect(() => {
-        // Update the initial zoom level whenever it changes
-        // const newScale = imageContainerWidth / imageWidth;
-        const newScale = 1
-        setInitialZoom(prevZoom => prevZoom * newScale);
-    }, [imageContainerWidth, imageWidth]);
-
-    // Zooms out to the value of 11
+    // zooms out to the value of 11
     const handleZoomOut = () => {
-        setInitialZoom(prevZoom => prevZoom - 8);
+        setInitialZoom(prevZoom => prevZoom - 11); 
     };
-
+    
     return (
         <div>
-        <div className="flex justify-center w-screen">
-            <h1> Counter-Strike </h1>
-        </div>
-        <div className="flex justify-center w-screen ">
-            <div className="relative max-w-lg overflow-hidden border-4 border-gray-700 h-max">
-            <Image
-                src="/images/awp/cu_medieval_dragon_awp.png"
-                height={4096}
-                width={4096}
-                quality={100}
-                alt="Dragon Lore"
-                priority={true}
-                style={imageStyle}
-            />
+            <div className="flex justify-center w-screen">
+                <h1> Counter-Strike </h1>
             </div>
-            <div className="flex justify-center">
-            <button onClick={handleZoomOut} className="px-4 py-2 text-white bg-red-500">
-                Zoom Out
-            </button>
+            <div className="flex justify-center w-screen ">
+                <div className="relative max-w-screen-sm overflow-hidden border-4 border-gray-700 h-max">
+                    <Image
+                    src="/images/awp/cu_medieval_dragon_awp.png"
+                    height={4096}
+                    width={4096}
+                    quality={100}
+                    alt="Dragon Lore"
+                    priority={true}
+                    style={imageStyle}
+                    />   
+                </div>
+                <div className="flex justify-center">
+                    <button onClick={handleZoomOut} className="px-4 py-2 text-white bg-red-500">
+                        Zoom Out
+                    </button>
+                </div>
             </div>
-        </div>
         </div>
     );
-}
+  }
