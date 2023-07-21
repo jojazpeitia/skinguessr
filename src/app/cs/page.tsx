94b69dac392f610fb5d4266a0c737d5dee02087a
jojazpeitia <jojazpeitia@gmail.com>
2023-07-21 01:40:39 -0500

    added Rajdhani font to text

--- src/app/cs/page.tsx
+++ src/app/cs/page.tsx
@@ -4,6 +4,10 @@ import { useState } from 'react';
 import Image from 'next/image';
 import { Input } from "@/components/ui/input";
 
+import { Rajdhani } from 'next/font/google'
+
+const rajdhani = Rajdhani({ subsets: ['latin'], weight: ['500'] });
+
 export default function CSPage() {
     
     // sets initial zoom to a value of 28.
@@ -25,8 +29,8 @@ export default function CSPage() {
     };
     
     return (
-        <div className="flex flex-col items-center justify-center mx-20 space-y-5 md:space-x-20 md:flex-row md:space-y-0">
-            <div className="max-w-screen-sm overflow-hidden border-4 border-gray-700 rounded shadow-sm absoluite h-max">
+        <div className="flex flex-col items-center justify-center mx-20 space-y-5 md:space-x-20 md:flex-row md:space-y-0 md:min-w-fit">
+            <div className="relative overflow-hidden border-4 border-gray-700 rounded shadow-sm h-96 w-96 ">
                 <Image
                 src="/images/awp/cu_medieval_dragon_awp.png"
                 height={4096}
@@ -38,8 +42,13 @@ export default function CSPage() {
                 />   
             </div>
             <div className='text-center '>
-                <h1 className='mb-2'> Place your guess: </h1>
+                <div className={rajdhani.className}>
+                    <h1 className='mb-2 text-xl'> Place your guess: </h1>
+                </div>
                 <Input className='shadow-sm w-96'/>
+                <button onClick={handleZoomOut} className="px-4 py-2 mt-4 text-white bg-red-500 rounded-md">
+                        Zoom Out
+                </button>
             </div>
         </div>
     );
