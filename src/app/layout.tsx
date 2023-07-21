bd84ed1b683c2c10b7e1b3920e8e34ab3efd7aad
jojazpeitia <jojazpeitia@gmail.com>
2023-07-21 13:02:45 -0500

    implemented fonts globally

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,17 @@
-import './globals.css'
+import './globals.css';
 import NavBar from './components/NavBar';
+import { Rajdhani, Inter } from 'next/font/google';
+
+const rajdhani = Rajdhani({
+  weight: ['500'],
+  subsets: ['latin'],
+  variable: '--font-rajdhani',
+});
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
 
 export const metadata = {
   title: 'SkinGuessr',
@@ -14,8 +26,10 @@ export default function RootLayout({
   return (   
     <html lang="en">
       <body>
-          <NavBar />
-          <main className='mt-10'>{children}</main>
+          <div className={`${inter.className}`}>
+            <NavBar />
+          </div>
+          <main className={`${rajdhani.variable} font-sans mt-12`}>{children}</main>
       </body>
     </html>
   );
