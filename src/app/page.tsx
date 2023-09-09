27049fa794b1828c309e51841ef244539dd381c4
jojazpeitia <jojazpeitia@gmail.com>
2023-09-08 23:51:25 -0500

    set up cypress e2e + wrote basic integration test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <motion.div 
+    <motion.h1
       className="flex justify-center w-screen text-xl italic"
       initial={{opacity: 0, y:20}} animate={{opacity:1, y:0, transition: {delay: 0.3}}} exit={{opacity: 0, y:20}}
     > 
         Home
-    </motion.div>
+    </motion.h1>
   );
 }
 
